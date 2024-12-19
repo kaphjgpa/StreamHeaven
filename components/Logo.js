7316ba1746b4034e843fdc3473883f79820459d0
Surendra singh <surendrasinghc80@gmail.com>
2024-12-20 02:31:49 +0530

    thin scrollbar is added

--- components/Logo.js
+++ components/Logo.js
@@ -12,7 +12,7 @@ export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
       <Image src="/Logo.png" height="40" width="40" alt="Logo" />
-      <p className={cn("font-semibold", font.className)}>STREAM HEAVEN</p>
+      <p className={cn("font-normal", font.className)}>STREAM HEAVEN</p>
     </div>
   );
 };
